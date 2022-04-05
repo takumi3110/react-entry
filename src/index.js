2ff89d4cab4445477e3ba60b67e3ee10334caeb4
takumi3110 <57779205+takumi3110@users.noreply.github.com>
2022-04-05 14:00:57 +0900

    practice1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import "./styles.css";
+// const let
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var";
+// console.log(val1);
+
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = 'let変数';
+// console.log(val2);
+
+// val2 = 'let変数を上書き';
+// console.log(val2);
+
+// let val2 = 'let変数を再宣言';
+
+// const val3 = 'const変数';
+// console.log(val3);
+
+// // val3 = 'const変数を上書き';
+
+// // const val3 = 'const変数を再宣言';
+
+// const val4 = {
+//   name: 'name',
+//   age: 28
+// };
+
+// // console.log(val4);
+// val4.name = 'new name';
+// val4.address = 'address';
+// console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+
+console.log(val5);
